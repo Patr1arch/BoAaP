090e448402c5a4596dd1c2303b3783fdef7eb448
Nikita Baranov <43929725+Patr1arch@users.noreply.github.com>
2023-04-05 13:08:34 +1000

    [Поработать с санитайзером] Вынес тестовые элементы в функции

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include "libs/heap-sort.c"
-#include "libs/list.c"
+#include "libs/heap-sort-min.c"
+#include "libs/hash_table.c"
 
 void array_init(int* arr) {
     *(arr + 0) = 4;
@@ -20,23 +21,27 @@ void array_print(int* arr, int arr_lenght) {
     printf("\n\n\n");
 }
 
-int main(int argc, char** argv) {
-    //int arr_size = 6;
-    //int arr_lenght = arr_size - 1;
+void test_heaps() {
+    int arr_size = 6;
+    int arr_lenght = arr_size - 1;
 
-    //int* arr = (int *) malloc(sizeof(int) * arr_size);
+    int* arr = (int *) malloc(sizeof(int) * arr_size);
 
-    //array_init(arr);
-    //array_print(arr, arr_lenght);
+    array_init(arr);
+    array_print(arr, arr_lenght);
 
-    //heap_sort(arr, arr_lenght);
+    max_heap_sort(arr, arr_lenght);
 
-    //array_print(arr, arr_lenght);
+    array_print(arr, arr_lenght);
 
-    // for (int i = 0; i <= arr_lenght; i++) {
-    //     free((arr + i));
-    // }
+    min_heap_sort(arr, arr_lenght);
 
+    array_print(arr, arr_lenght);
+
+    free(arr);
+}
+
+void test_lists() {
     List* list = (List*) malloc(sizeof(List));
     bool res = find(list, 42);
     push(list, 42);
@@ -48,8 +53,27 @@ int main(int argc, char** argv) {
     list_print(list);
     deb = pop(list);
     list_print(list);
+}
 
-    res = find(list, 42);
+void test_hash_table() {
+    HashTable* table = init_hash_table(10);
+    printf("%d\n", find_element(table, 42));
+    add_element(table, 42);
+    add_element(table, 52);
+    add_element(table, 42);
+    
+    printf("%d\n", find_element(table, 42));
+    list_print((table->array+2));
+
+    delete_hash_table(table);
+}
+
+int main(int argc, char** argv) {
+    test_heaps();
+
+    test_lists();
+
+    test_hash_table();
 
     return 0;
 }
